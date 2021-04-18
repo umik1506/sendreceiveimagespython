c734bd55abb39669bf2129219ad677b3fc6f07c4
umik1506 <80735754+umik1506@users.noreply.github.com>
2021-04-18 22:13:45 +0700

    Add files via upload

---
+++ server.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, Response
+import jsonpickle
+import numpy as np
+import cv2
+import os
+
+simpan = '/home/umikolif/mysite'
+
+# Initialize the Flask application
+app = Flask(__name__)
+app.config['simpan'] = simpan
+
+# route http posts to this method
+@app.route('/api/test', methods=['POST'])
+def test():
+    r = request
+    # convert string of image data to uint8
+    nparr = np.fromstring(r.data, np.uint8)
+    # decode image
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    # do some fancy processing here....
+    cv2.imwrite(os.path.join(app.config['simpan'] , 'vw.jpg'), img)
+
+    # build a response dict to send back to client
+    response = {'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0])}
+    # encode response using jsonpickle
+    response_pickled = jsonpickle.encode(response)
+
+    return Response(response=response_pickled, status=200, mimetype="application/json")
+
+
+# start flask app
+# app.run(host="0.0.0.0", port=5000)
